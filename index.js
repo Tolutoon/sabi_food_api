fd67cc899f73636f8ba004d39aa2dd72a3765ab2
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2024-07-11 11:11:08 +0100

    push

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose.connect('mongodb+srv://getolopadetolu:UeHo5pdSnYlGJIaw@sabifood.fdqz03j
 
 app.get('/', (req, res)=> {
 
-    res.send('Hi Tolutoon');
+    res.send('Hi, Welcome to Sabi Food API DOC');
     console.log(`App is running on ${PORT}`);
 });
 
@@ -29,6 +29,17 @@ app.post('/api/restaurants', async (req, res) => {
 app.get('/api/restaurants', async (req, res) => {
     try {
         const restaurant = await Restaurant.find({});
+        res.status(200).json(restaurant);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+
+app.get('/api/restaurant/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const restaurant = await Restaurant.findById(id);
         res.status(200).json(restaurant);
     } catch (error) {
         res.status(500).json({message: error.message});
